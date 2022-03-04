db9d51e7d4583092342b02e004f7e5737add9698
megatank58 <tanumegatank@gmail.com>
2022-03-04 14:56:59 +0530

    style: code cleanup

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 
 func main() {
 	app := minima.New()
+	app.UseRouter(routes.Router())
+	app.UseRaw(middleware.Logger)
+
 	crs := cors.New()
 	app.Use(crs.NewCors(cors.Options{
 		AllowedOrigins:   []string{"https://localhost:3000"},
 		AllowCredentials: true,
 		Debug:            true,
 	}))
-	app.UseRaw(middleware.Logger)
-	app.UseRouter(routes.Router())
+
 	app.Listen(":3000")
 }
