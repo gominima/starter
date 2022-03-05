835653575c78bc9faefa0dee51f497953d3ee8dc
megatank58 <tanumegatank@gmail.com>
2022-03-05 11:49:25 +0530

    chore: cors section

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main;
 import (
 	"github.com/gominima/minima"
 	"github.com/gominima/middlewares"
+	"github.com/gominima/cors"
 	"github.com/gominima/starter/routes"
 )
 
 func main() {
 	app := minima.New()
+	cors := cors.New()
 	app.UseRouter(routes.Router())
 	app.UseRaw(middlewares.Logger())
+	app.Use(cors.NewCors(cors.Options{
+        	AllowedOrigins: []string{"http://example.com"},
+        	AllowCredentials: true,
+        	// Enable Debugging for testing, consider disabling in production
+        	Debug: true,
+	})())
 	app.Listen(":3000")
 }
